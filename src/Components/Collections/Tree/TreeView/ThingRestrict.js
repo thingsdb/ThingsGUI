88bb82ee53d9ebbd1bac4738e6e6a60f7e29ebb0
AnjaBruls <anja@transceptor.technology>
2020-03-04 17:55:13 +0100

    update doc & added dictionary

--- src/Components/Collections/Tree/TreeView/ThingRestrict.js
+++ src/Components/Collections/Tree/TreeView/ThingRestrict.js
@@ -8,7 +8,7 @@ import React from 'react';
 
 import ThingsBounds from './ThingsBounds';
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles(() => ({
     justifyContent: {
         justifyContent: 'center',
     }
