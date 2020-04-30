81956fb188839dc5d1402260ac172ccc46600863
AnjaBruls <anja@transceptor.technology>
2020-04-30 16:32:41 +0200

    sort function tables landingpage

--- src/Components/Util/StickyHeadTable.js
+++ src/Components/Util/StickyHeadTable.js
@@ -8,6 +8,7 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TablePagination from '@material-ui/core/TablePagination';
 import TableRow from '@material-ui/core/TableRow';
+import TableSortLabel from '@material-ui/core/TableSortLabel';
 
 const useStyles = makeStyles({
     container: {
@@ -15,18 +16,53 @@ const useStyles = makeStyles({
     },
 });
 
+const descendingComparator = (a, b, orderBy) => {
+    if (b[orderBy] < a[orderBy]) {
+        return -1;
+    }
+    if (b[orderBy] > a[orderBy]) {
+        return 1;
+    }
+    return 0;
+};
+
+const getComparator = (order, orderBy) => {
+    return order === 'desc'
+        ? (a, b) => descendingComparator(a, b, orderBy)
+        : (a, b) => -descendingComparator(a, b, orderBy);
+};
+
+const stableSort = (array, comparator) => {
+    const stabilizedThis = array.map((el, index) => [el, index]);
+    stabilizedThis.sort((a, b) => {
+        const order = comparator(a[0], b[0]);
+        if (order !== 0) return order;
+        return a[1] - b[1];
+    });
+    return stabilizedThis.map((el) => el[0]);
+};
+
 const StickyHeadTable = ({columns, rows, size}) => {
     const classes = useStyles();
     const [page, setPage] = React.useState(0);
     const [rowsPerPage, setRowsPerPage] = React.useState(4);
+    const [order, setOrder] = React.useState('asc');
+    const [orderBy, setOrderBy] = React.useState(columns[0]?columns[0].id:'');
 
-    const handleChangePage = (event, newPage) => {
+    const handleChangePage = (newPage) => {
         setPage(newPage);
     };
 
-    const handleChangeRowsPerPage = (event) => {
-        setRowsPerPage(+event.target.value);
+    const handleChangeRowsPerPage = ({target}) => {
+        const {value} = target;
+        setRowsPerPage(+value);
         setPage(0);
+    };
+
+    const handleRequestSort = (property) => () => {
+        const isAsc = orderBy === property && order === 'asc';
+        setOrder(isAsc ? 'desc' : 'asc');
+        setOrderBy(property);
     };
 
     return (
@@ -41,13 +77,19 @@ const StickyHeadTable = ({columns, rows, size}) => {
                                     align={column.align}
                                     style={{ minWidth: column.minWidth }}
                                 >
-                                    {column.label}
+                                    <TableSortLabel
+                                        active={orderBy === column.id}
+                                        direction={orderBy === column.id ? order : 'asc'}
+                                        onClick={handleRequestSort(column.id)}
+                                    >
+                                        {column.label}
+                                    </TableSortLabel>
                                 </TableCell>
                             ))}
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                        {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
+                        {stableSort(rows, getComparator(order, orderBy)).slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
                             return (
                                 <TableRow hover role="checkbox" tabIndex={-1} key={row.title}>
                                     {columns.map((column) => {
