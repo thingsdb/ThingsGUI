c91f831432eceb0b2490cb785075e94c0a1236ab
AnjaBruls <anja@transceptor.technology>
2021-01-11 11:05:19 +0100

    update

--- src/Components/Editor/EditorInput.js
+++ src/Components/Editor/EditorInput.js
@@ -62,9 +62,12 @@ const Editor = ({height, history, input, match, onExpand, onOutput, scope}) => {
     };
 
     const handleSubmit = () => {
-        // onExpand(false);
-        // onOutput(null);
-        CollectionActions.rawQuery(scope, query, tag, handleOutput);
+        onExpand(false);
+        setTimeout(()=> { // can result in -> Warning: Can't perform a React state update on an unmounted component. This is a no-op, but it indicates a memory leak in your application. To fix, cancel all subscriptions and asynchronous tasks in a useEffect cleanup function. in Editor (created by WithVlow()(Editor)) in WithVlow()(Editor) (created by App)
+            onOutput(null);
+            CollectionActions.rawQuery(scope, query, tag, handleOutput);
+        }, 100);
+
         EditorActions.setHistory(query);
     };
 
@@ -114,7 +117,7 @@ const Editor = ({height, history, input, match, onExpand, onOutput, scope}) => {
     return (
         <React.Fragment>
             <div onKeyUp={handleKeyUp} onKeyDown={handleKeyDown}>
-                <QueryInput onChange={handleInput} input={queryInput} height={height-75} />
+                <QueryInput onChange={handleInput} input={queryInput} height={height-80} />
             </div>
             <CardActions className={classes.cardAction} disableSpacing>
                 <Button
