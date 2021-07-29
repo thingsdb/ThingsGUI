61406c2e834fbef7e34e947a905830b27738954a
AnjaBruls <anja@transceptor.technology>
2021-07-29 18:18:17 +0200

    adjust padding

--- src/Components/Util/TitlePage.js
+++ src/Components/Util/TitlePage.js
@@ -1,3 +1,4 @@
+import {makeStyles} from '@material-ui/core/styles';
 import React from 'react';
 import PropTypes from 'prop-types';
 import Card from '@material-ui/core/Card';
@@ -5,10 +6,16 @@ import CardContent from '@material-ui/core/CardContent';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 
-const TitlePage = ({preTitle, title, content}) => {
+const useStyles = makeStyles(theme => ({
+    bottom: {
+        marginBottom: theme.spacing(6)
+    },
+}));
 
+const TitlePage = ({preTitle, title, content}) => {
+    const classes = useStyles();
     return (
-        <Grid container spacing={1}>
+        <Grid className={classes.bottom} container spacing={1}>
             <Grid item xs={12}>
                 <Card>
                     <CardContent>
--- src/Components/Util/TitlePage3.js
+++ src/Components/Util/TitlePage3.js
@@ -1,3 +1,4 @@
+import {makeStyles} from '@material-ui/core/styles';
 import React from 'react';
 import PropTypes from 'prop-types';
 import Card from '@material-ui/core/Card';
@@ -5,10 +6,16 @@ import CardContent from '@material-ui/core/CardContent';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 
-const TitlePage3 = ({preTitle, title, content}) => {
+const useStyles = makeStyles(theme => ({
+    bottom: {
+        marginBottom: theme.spacing(6)
+    },
+}));
 
+const TitlePage3 = ({preTitle, title, content}) => {
+    const classes = useStyles();
     return (
-        <Grid container spacing={1}>
+        <Grid container spacing={1} className={classes.bottom}>
             <Grid item xs={12}>
                 <Card>
                     <CardContent>
